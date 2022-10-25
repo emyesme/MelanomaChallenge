08ecacae248cd354b73043ff8d84732939385abc
emyesme <emily.carvajal@correounivalle.edu.co>
2022-10-26 01:14:41 +0200

    threshold and pipelines working some features to

---
+++ features/check.sh
@@ -0,0 +1,9 @@
+#!bin/bash
+echo "features ch1 test d "
+cat featuresCh1TestD.csv | wc -l
+echo "features ch2 train b "
+cat featuresCh2TrainB.csv | wc -l
+echo "features ch2 train d"
+cat featuresCh2TrainD.csv | wc -l
+echo "features ch2 test d"
+cat featuresCh2TestD.csv | wc -l
